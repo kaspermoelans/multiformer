8b69dca771e19ca18fdab232ecc1d301f0532bce
kaspermoelans <104373919+kaspermoelans@users.noreply.github.com>
2024-04-09 11:54:49 +0200

    Add port

--- public/index.js
+++ public/index.js
@@ -9,7 +9,7 @@ canvasEl.width = window.innerWidth;
 canvasEl.height = window.innerHeight;
 const canvas = canvasEl.getContext("2d");
 
-const socket = io(`ws://localhost:5000`);
+const socket = io();
 
 let map = [[]];
 let players = []
